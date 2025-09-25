34c787834c49a234911db9f283a18be10e74dde9
Rami <138556019+rami242424@users.noreply.github.com>
2025-09-25 11:38:24 +0900

    vite-env edit

--- k-beauty/src/api/config.ts
+++ k-beauty/src/api/config.ts
@@ -1,5 +1,5 @@
 // 상품/카테고리용 외부 API
-export const PRODUCT_API_BASE_URL =
+export const PRODUCT_API_URL =
   import.meta.env.VITE_PRODUCT_API_URL || "https://dummyjson.com";
 
 // 회원가입/로그인/장바구니용 내 백엔드 API
